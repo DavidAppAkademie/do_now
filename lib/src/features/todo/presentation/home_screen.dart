01b8b726a0a4db19c78f8abbb0218af4f5106e2d
David Sent <david.sent@app-akademie.com>
2025-05-05 09:45:10 +0200

    add folder structure

---
+++ lib/src/features/todo/presentation/home_screen.dart
@@ -0,0 +1,99 @@
+import 'package:do_now/src/features/todo/presentation/widgets/date_container.dart';
+import 'package:do_now/src/features/todo/presentation/widgets/todo_card.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Padding(
+          padding: EdgeInsets.fromLTRB(16, 16, 16, 0),
+          child: Column(
+            spacing: 32,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "DoNow",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                  Text(
+                    "Jun",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  DateContainer(day: "19", isToday: true),
+                  DateContainer(day: "20", isToday: false),
+                  DateContainer(day: "21", isToday: false),
+                  DateContainer(day: "22", isToday: false),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.chevron_right,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              Image.asset(
+                'assets/images/calendar.png',
+                height: 130,
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.blueAccent,
+        toolbarHeight: 300,
+        shadowColor: Colors.blueAccent,
+        elevation: 16,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(80))),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(16, 32, 16, 16),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 16,
+            children: [
+              TodoCard(
+                title: "13:00-15:00",
+                subTitle: "UI Design Webinar",
+                icon: Icons.palette_outlined,
+                color: Colors.blue,
+              ),
+              TodoCard(
+                title: "16:00-17:00",
+                subTitle: "Snacks Time",
+                icon: Icons.lunch_dining,
+                color: Colors.pink,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
