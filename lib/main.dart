ed6d7d73012f66c6b15cbc8f0ce3605e81c149bc
David Sent <david.sent@app-akademie.com>
2025-07-02 09:12:11 +0200

    firebase installed

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:do_now/firebase_options.dart';
 import 'package:do_now/src/app.dart';
 import 'package:do_now/src/data/database_repository.dart';
 import 'package:do_now/src/data/mock_database_repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 void main() async {
-  await initializeDateFormatting('de_DE', null);
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await initializeDateFormatting('de_DE');
 
   final DatabaseRepository db = MockDatabaseRepository();
 
